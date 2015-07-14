f44fc3fcafe6b2bdbe537f725b4c558ef4a9e6bf
Alex Soveliev <Suroviy@bk.ru>
2015-07-14 18:30:05 +0300

    Create cde.js

---
+++ cde.js
@@ -0,0 +1,135 @@
+function createDOMelement (config) {
+    this.arrayToString = function (val,dimer){
+        var el = '';
+        if (typeof dimer !== 'string'){
+            dimer = ' ';
+        }
+        if (typeof val === 'string'){
+            el = val;
+        }
+        if (typeof val === 'object'){
+            val.forEach(function(item,i,arr){
+                el = el+item;
+                if (arr.length > i+1){
+                    el = el+dimer;
+                }
+            });
+        }
+        return el;
+    }
+    
+    this.bindToString = function (val){
+    
+        if (typeof val === 'object'){
+            var bind = [];
+            if (typeof val.forEach !== 'undefined'){
+                
+                val.forEach(function(item){
+                    if (typeof item === 'object'){
+    
+                        bind.push(this.bindToString(item));
+                    }else{
+                        bind.push(item);
+                    }
+                });
+                return '['+arrayToString(bind,',')+']';
+            }else{
+                for(var p in val) {
+                    if (typeof val[p] === 'object'){
+                        bind.push("'"+p+"':"+this.bindToString(val[p]));
+                    }else{
+                        bind.push("'"+p+"':'"+val[p]+"'");
+                    }
+                }
+                return '{'+arrayToString(bind,',')+'}';
+            }
+        }
+    
+        return '{}';
+    }
+
+    this.create =  function (config){
+        if (typeof config.tag === 'string'){
+            var el = document.createElement(config.tag);
+        }else{
+            var el = document.createElement('div');
+        }
+        
+        /*bind*/    
+        if (typeof config.bind === 'object'){
+            el.setAttribute('data-bind', this.bindToString(config.bind));
+        }
+    
+        /*class*/    
+        if (typeof config.class !== 'undefined'){
+            el.className = this.arrayToString(config.class);
+        }
+        
+        if (typeof config.style === 'object'){
+            for(var p in config.style) {
+                el.style[p] = config.style[p];
+            }
+        }
+        
+        /*html*/
+        if (typeof config.html === 'string'){
+            el.innerHTML = config.html;
+        }else{
+            /*inner*/
+            if (typeof config.inner === 'object'){
+                if ( typeof config.inner.forEach !== 'undefined'){
+    
+                config.inner.forEach(function(item){
+                    el.appendChild(this.create(item));
+                });
+    
+                }else{
+                    el.appendChild(config.inner);
+                }
+            }
+        }    
+        return el;
+    }
+    return this.create(config);
+}
+
+function create(config){
+    return createDOMelement(config);
+}
+
+
+var elem = create({
+	tag: 'div',
+    class: ['alert','info'],
+    bind: {
+        value:'mail',
+        text: 'sss',
+        options:[
+            {
+                do: [
+                    {
+                    ha:'asd'
+                    }
+                ],
+        ded: {gov:'sdfdf'}
+            }   
+        ]
+    },
+    inner: [
+        {
+		    tag: 'p',
+            html: 'adf',
+        },{
+            tag:'h1',
+            style:{
+                color:'blue',
+                'font-size': '49px'
+            },
+            html:'234'
+        }
+    ]
+});
+
+//alert(go.toString());
+main.innerHTML = null;
+main.appendChild(elem);
